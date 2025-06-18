5114a39b7aece41e6d47ece497165a8c80823202
dominguez1-m <roda060309@gmail.com>
2025-06-17 20:02:41 -0600

    Create actualizar.php

---
+++ actualizar.php
@@ -0,0 +1,24 @@
+<?php
+$conexion = new mysqli("localhost", "root", "", "nombre_de_tu_base");
+
+$id = $_POST['id'];
+$nombre = $_POST['nombre'];
+$barrio = $_POST['barrio'];
+$edad = $_POST['edad'];
+$genero = $_POST['genero'];
+$conformidad = $_POST['conformidad'];
+$comentarios = $_POST['comentarios'];
+
+$sql = "UPDATE comentarios SET
+    nombre = '$nombre',
+    barrio = '$barrio',
+    edad = $edad,
+    genero = '$genero',
+    conformidad = '$conformidad',
+    comentarios = '$comentarios'
+    WHERE id = $id";
+
+$conexion->query($sql);
+
+header("Location: consultas.php");
+?>
