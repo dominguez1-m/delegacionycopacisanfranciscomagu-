37a57008ffa07812d9c100db953d1a2587c05150
dominguez1-m <roda060309@gmail.com>
2025-06-17 20:01:59 -0600

    Create modificaciones.php

---
+++ modificaciones.php
@@ -0,0 +1,35 @@
+<?php
+$conexion = new mysqli("localhost", "root", "", "nombre_de_tu_base");
+
+$id = $_GET['id'];
+$sql = "SELECT * FROM comentarios WHERE id = $id";
+$resultado = $conexion->query($sql);
+$fila = $resultado->fetch_assoc();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Editar Comentario</title>
+</head>
+<body>
+    <h2>Editar Comentario</h2>
+    <form action="actualizar.php" method="POST">
+        <input type="hidden" name="id" value="<?= $fila['id'] ?>">
+
+        Nombre: <input type="text" name="nombre" value="<?= $fila['nombre'] ?>"><br>
+        Barrio:
+        <input type="text" name="barrio" value="<?= $fila['barrio'] ?>"><br>
+        Edad: <input type="number" name="edad" value="<?= $fila['edad'] ?>"><br>
+        Género:
+        <input type="text" name="genero" value="<?= $fila['genero'] ?>"><br>
+        Conformidad:
+        <input type="text" name="conformidad" value="<?= $fila['conformidad'] ?>"><br>
+        Comentarios:<br>
+        <textarea name="comentarios"><?= $fila['comentarios'] ?></textarea><br>
+
+        <input type="submit" value="Actualizar">
+    </form>
+</body>
+</html>
