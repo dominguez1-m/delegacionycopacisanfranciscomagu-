fa025d6758e0cf26fc5c9214f348900cd0637cde
dominguez1-m <roda060309@gmail.com>
2025-06-17 20:04:54 -0600

    Update eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "nombre_de_tu_base");
+$conexion = new mysqli("localhost", "root", "", "comentarios");
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
